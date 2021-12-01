927b349063c633d98cf92c13b001c2714c5baf2d
duarteocarmo <duarteocarmo@gmail.com>
2021-12-01 10:27:54 +0100

    day 1

--- 01/day_01.py
+++ 01/day_01.py
@@ -1,14 +1,11 @@
-import sys
-
 with open("01/input.txt") as file:
     lines = file.readlines()
     lines = [int(line.rstrip()) for line in lines]
 
+
+# PART 1
 increases = 0
-
 for index, value in enumerate(lines):
-
-    value = value
 
     if index == len(lines) - 1:
         break
@@ -19,3 +16,22 @@ for index, value in enumerate(lines):
         increases = increases + 1
 
 print(increases)
+
+# PART 2
+previous_sum = 99999999999999999999
+sum_increases = 0
+for index, value in enumerate(lines):
+
+    if index == len(lines) - 1:
+        break
+
+    first = lines[index - 1]
+    mid = lines[index]
+    next = lines[index + 1]
+    current_sum = first + mid + next
+    if current_sum > previous_sum:
+        sum_increases = sum_increases + 1
+
+    previous_sum = current_sum
+
+print(sum_increases)
